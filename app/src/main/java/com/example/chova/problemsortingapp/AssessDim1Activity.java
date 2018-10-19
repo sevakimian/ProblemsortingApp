9073ad06485b3fabd1942f6fc80b5551c1e0bbbe
sevak.hovakimian.19 <sevak.hovakimian.19@gmail.com>
2018-10-19 13:57:51 +0200

    14h00 19/10

--- app/src/main/java/com/example/chova/problemsortingapp/AssessDim1Activity.java
+++ app/src/main/java/com/example/chova/problemsortingapp/AssessDim1Activity.java
@@ -3,6 +3,7 @@ package com.example.chova.problemsortingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -47,15 +48,35 @@ public class AssessDim1Activity extends AppCompatActivity {
             this.defileurs[i].setMin( 0 );
             this.defileurs[i].setMax( 10 );
             this.defileurs[i].setMinimumWidth(70);
-            //this.defileurs[i].setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.MATCH_PARENT));
+            this.defileurs[i].setProgress(5);
+            this.defileurs[i].setLayoutParams(new LinearLayout.LayoutParams(900,LinearLayout.LayoutParams.WRAP_CONTENT));
 
 
             //initialisation du label
             this.afficheurs[i]= new TextView(this);
             this.lignes[i].addView( this.afficheurs[i] );
             this.afficheurs[i].setText( "5" );
-            this.afficheurs[i].setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.MATCH_PARENT));
+            this.afficheurs[i].setLayoutParams(new LinearLayout.LayoutParams(100,40));
             this.afficheurs[i].setMinHeight(50);
+
+            this.defileurs[i].setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int valeur, boolean b) {
+
+                    String string = Integer.toString(valeur);
+                    AssessDim1Activity.this.afficheurs[0].setText(string);
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+
+                }
+            });
 
             //this.defileurs[i].setLayoutParams( ViewGroup. );
 
